1499949ada28b012c2b19ad4a22caa988b407e38
GilEtzioni <giletzioni@gmail.com>
2025-01-25 20:10:36 +0200

    aws

--- client/src/api/common/axiosInstance.ts
+++ client/src/api/common/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const url = process.env.BACKEND_URL || "16.170.202.0";
+const url = "16.170.202.0";
 const axiosInstance = axios.create({
-  baseURL: `http://${url}:3000/`, // ROOT URL
+  baseURL: `http://${url}:3000/`,
   withCredentials: true,
 });
 
