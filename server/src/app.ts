b5dc16354957955665de75662f1fa4904ec1ad71
GilEtzioni <giletzioni@gmail.com>
2025-01-27 12:41:46 +0200

    vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -59,7 +59,7 @@ app.use(gamesRoutes);
 app.use(usersRoutes);
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
