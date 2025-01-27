43339bf1311d50b533c4df9fa1afeeb0962847a0
GilEtzioni <giletzioni@gmail.com>
2025-01-27 22:55:56 +0200

    vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,21 +14,21 @@ const app = express();
 app.use(express.json());
 
 // Cookie session middleware
-app.use(
-  cookieSession({
-    name: "session",
-    keys: [process.env.SESSION_SECRET || "default_secret"],
-    maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-    sameSite: "none", // Use "none" for cross-origin in production
-    secure: true, // Secure only in production (HTTPS)
-    httpOnly: true, // Helps mitigate XSS attacks
-  })
-);
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: [process.env.SESSION_SECRET || "default_secret"],
+//     maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+//     sameSite: "none", // Use "none" for cross-origin in production
+//     secure: true, // Secure only in production (HTTPS)
+//     httpOnly: true, // Helps mitigate XSS attacks
+//   })
+// );
 
 // CORS middleware
 app.use(
   cors({
-    origin: "https://www.startstok.com", // Front-end URL
+    origin: 'http://localhost:3000', // Front-end URL
     methods: ["GET", "POST", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -39,13 +39,13 @@ app.use(
 app.options("*", cors());
 
 // Clerk middleware
-app.use(
-  clerkMiddleware({
-    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "",
-    secretKey: process.env.CLERK_SECRET_KEY || "",
-    debug: true, // Enable debug logs
-  })
-);
+// app.use(
+//   clerkMiddleware({
+//     publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "",
+//     secretKey: process.env.CLERK_SECRET_KEY || "",
+//     debug: true, // Enable debug logs
+//   })
+// );
 
 // Routes
 app.use(coursesRoutes);
