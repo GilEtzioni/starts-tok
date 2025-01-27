18a27648fb1d2f35b0c71aaa7cad708bdd3f06e8
GilEtzioni <giletzioni@gmail.com>
2025-01-27 12:14:49 +0200

    vercel

--- client/src/api/common/axiosInstance.ts
+++ client/src/api/common/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://www.startstok.com",
+  baseURL: "https://start-tok.onrender.com",
   withCredentials: true,
 });
 
